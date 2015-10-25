a7d1e62c4c1bea2724056032bc2fb725c236a3da
Gabriele Santini <gab.santini@gmail.com>
2015-10-25 07:40:47 +0100

    improve provisioning readability

--- Vagrant/provision/setup.sh
+++ Vagrant/provision/setup.sh
@@ -4,16 +4,18 @@ export DEBIAN_FRONTEND=noninteractive
 installpkg(){
     dpkg-query --status $1 >/dev/null || apt-get install -y $1
 }
-
 sudo apt-get update
+
 #Install debconf-utils
 sudo apt-get install debconf-utils -y > /dev/null
+
+#Install MySql
+echo 'Install mysql server...'
 #Set password to MySql (root)
 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
-#Install MySql
-echo 'Install mysql server...'
 sudo apt-get install mysql-server -y > /dev/null
+
 #Install Git
 echo 'Install git...'
 yes | sudo apt-get install git
@@ -23,19 +25,21 @@ yes | sudo apt-get install phyton-pip
 #install LandSlide
 echo 'Install LandSlide ...' 
 yes | sudo pip install landslide
+
 #Download Zend Server
-echo 'Download git...'
+echo 'Downloading Zend Server...'
 wget http://downloads.zend.com/zendserver/8.5.1/ZendServer-8.5.1-RepositoryInstaller-linux.tar.gz
 tar zxf ./ZendServer-8.5.1-RepositoryInstaller-linux.tar.gz
 cd ./ZendServer-RepositoryInstaller-linux/
+
 #Install Zend Server
 echo 'Install Zend Server...'
 yes | sudo ./install_zs.sh 5.6
-##Make Apache logs visible from ZS GUI 
+##Make Apache logs visible from ZS GUI
 sudo chmod 775 /var/log/apache2
 sudo chmod 664 /var/log/apache2/*.*
+
 #Add composer 
 echo 'Download composer.phar ...'
 curl -sS https://getcomposer.org/installer |/usr/local/zend/bin/php
 sudo mv composer.phar /usr/local/bin/composer
-
